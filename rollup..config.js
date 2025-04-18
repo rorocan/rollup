35a7040ae4c8b337fa267ff7bea13584b2ce5dd6
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2025-04-18 11:44:22 -0500

    main3

---
+++ rollup..config.js
@@ -0,0 +1,9 @@
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'es', // Immediately Invoked Function Expression
+       sourceMap: true, // Enable source maps for easier debugging
+    }
+    
+}
